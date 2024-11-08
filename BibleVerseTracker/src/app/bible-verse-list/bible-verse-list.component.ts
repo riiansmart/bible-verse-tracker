204956811cc3ea059ede5a2cd6d62f248fbee1dc
riiansmart <rismart1221@outlook.com>
2024-11-07 17:54:14 -0700

    implemented CRUD operations in bible-verse-list.component

--- BibleVerseTracker/src/app/bible-verse-list/bible-verse-list.component.ts
+++ BibleVerseTracker/src/app/bible-verse-list/bible-verse-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BibleVerseService } from '../service/bible-verse.service';
 
 @Component({
   selector: 'app-bible-verse-list',
-  standalone: false,
   templateUrl: './bible-verse-list.component.html',
-  styleUrl: './bible-verse-list.component.scss'
+  styleUrls: ['./bible-verse-list.component.scss']
 })
-export class BibleVerseListComponent {
+export class BibleVerseListComponent implements OnInit {
+  verses: any[] = [];
 
+  constructor(private bibleVerseService: BibleVerseService) { }
+
+  ngOnInit(): void {
+    this.bibleVerseService.getAllVerses().subscribe((data) => {
+      this.verses = data;
+    });
+  }
+
+  deleteVerse(id: number): void {
+    this.bibleVerseService.deleteVerse(id).subscribe(() => {
+      this.verses = this.verses.filter(verse => verse.id !== id);
+    });
+  }
 }
