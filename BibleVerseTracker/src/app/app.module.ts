76e745c35f7474e288fa68f699c581f17153bf47
riiansmart <rismart1221@outlook.com>
2024-11-07 18:14:32 -0700

    added in favorite component

--- BibleVerseTracker/src/app/app.module.ts
+++ BibleVerseTracker/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { BibleVerseListComponent } from './bible-verse-list/bible-verse-list.component';
 import { BibleVerseCreateComponent } from './bible-verse-create/bible-verse-create.component';
 import { BibleVerseUpdateComponent } from './bible-verse-update/bible-verse-update.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BibleVerseListComponent,
     BibleVerseCreateComponent,
-    BibleVerseUpdateComponent
+    BibleVerseUpdateComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
---
+++ BibleVerseTracker/src/app/favorite/favorite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  standalone: false,
+  templateUrl: './favorite.component.html',
+  styleUrl: './favorite.component.scss'
+})
+export class FavoriteComponent {
+
+}
