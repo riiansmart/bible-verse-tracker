79d532d5694f46c8e1c28fc3256b9c43f451097f
riiansmart <rismart1221@outlook.com>
2024-11-08 12:20:15 -0700

    changed port to 3000 for api

--- BibleVerseTracker-API/src/index.ts
+++ BibleVerseTracker-API/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/api', bibleVerseRoutes);
 app.use('/api', favoriteRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
